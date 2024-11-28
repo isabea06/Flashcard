7381d6687f247b2121cdec90396cdce256cad489
isabea06 <00001098626291sp@al.educacao.sp.gov.br>
2024-11-27 22:47:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Fisica',
+    'Quem descobriu a força da "Lei Gravitacional"?',
+    'Isaac Newton'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual é o resultado dessa multiplicação 4552x82? ',
+    '373.264'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é maior orgão do corpo humano?',
+    'Pele'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a maior bacia hidrográfica do mundo?',
+    'Bacia Amazônica'
+)
+
+criaCartao(
+    'Historia',
+    'Quando começou a guerra de canudo?',
+    '7 de novembro de 1896'
+)
+
+criaCartao(
+    'Português',
+    'Quem foi o maior escrito do Brasil?',
+    'Joaquim Maria Machado de Assis'
+)
